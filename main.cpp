f4f7ed540e0f45a340e9a1fbb2436f38a65d2ac2
Nekekys <geri_337@mail.ru>
2022-04-01 14:16:05 +0300

    -

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 	int a,b;
     std::cout << "Enter A and B: ";
     std::cin >> a >> b;
-    std::cout << "A + B = " << a + b <<'\n'; //комментарий
+    std::cout << "A + B = " << a + b <<'\n'; // комментарий
     std::cout << "A - B = " << a - b <<'\n';
     std::cout << "A / B = " << a / b <<'\n';
 }
