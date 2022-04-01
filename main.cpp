75c4e00a864c74a02f1920a41913b8160d5f7740
Nekekys <geri_337@mail.ru>
2022-04-01 13:26:57 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "std.h"
+ 
+
+
+ 
+int main()
+{
+	int a,b;
+    std::cout << "Enter A and B: ";
+    std::cin >> a >> b;
+    std::cout << "A + B = " << a + b <<'\n';
+    std::cout << "A - B = " << a - b <<'\n';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
